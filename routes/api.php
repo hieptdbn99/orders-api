c87aa7faaecbc97b82cbb4429892cf3cb18cd586
hieptdbn99 <tdhmesut@gmail.com>
2021-09-24 09:12:04 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('orders', \App\Http\Controllers\Api\OrderController::class);
+
